6114c8df0cf360b114f7133f1d79d282cd6d2384
Baurain Olivier <baurain.o@gmail.com>
2021-05-24 00:05:37 +0200

    feat(middleware): add toomanyrequests message error

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -1,9 +1,14 @@
+const { TooManyRequests } = require('@feathersjs/errors');
 const rateLimit = require('express-rate-limit');
 
 module.exports = function (app) {
+  const handler = function () {
+    throw new TooManyRequests('Too many requests, please try again later.');
+  };
   const limiter = rateLimit({
     windowMs: 10 * 1000, // 10 seconds
     max: 10, // limit each IP to 10 requests per windowMs
+    handler,
   });
   app.use(limiter);
 };
