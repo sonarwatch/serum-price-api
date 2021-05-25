69ce0863e7fd9fae01dd23a7dca22793454b35f1
Baurain Olivier <baurain.o@gmail.com>
2021-05-25 11:14:15 +0200

    feat(markets): add stable coins

--- src/services/prices/prices.class.js
+++ src/services/prices/prices.class.js
@@ -5,13 +5,33 @@ const { Service } = require('feathers-memory');
 const logger = require('../../logger');
 const sleep = require('../../utils/sleep');
 const markets = require('../../utils/markets.json');
+const stableCoins = require('../../utils/stableCoins.json');
 
 exports.Prices = class Prices extends Service {
   setup(app) {
     this.connection = app.get('connection');
   }
 
+  async addStableCoins() {
+    try {
+      for (let i = 0; i < stableCoins.length; i += 1) {
+        const stableCoin = stableCoins[i];
+        const price = {
+          id: stableCoin.tokenMint,
+          mint: stableCoin.tokenMint,
+          symbol: stableCoin.symbol,
+          price: stableCoin.price,
+          market: stableCoin.marketId,
+        };
+        await this.create(price);
+      }
+    } catch (error) {
+      logger.error('[addStableCoins]', error);
+    }
+  }
+
   async updateAll() {
+    await this.addStableCoins();
     for (let i = 0; i < markets.length; i += 1) {
       const baseMarketObj = markets[i];
       const marketAddress = new PublicKey(baseMarketObj.marketId);
